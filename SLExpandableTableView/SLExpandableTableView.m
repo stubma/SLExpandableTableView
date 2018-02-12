638ba0e3342949d0fd353b52f22a4ecc003aafab
luma <stubma@gmail.com>
2018-02-12 15:10:37 +0800

    remove scroll

--- SLExpandableTableView/SLExpandableTableView.m
+++ SLExpandableTableView/SLExpandableTableView.m
@@ -306,10 +306,6 @@ static BOOL protocol_containsSelector(Protocol *protocol, SEL selector)
 
     [self.animatingSectionsDictionary removeObjectForKey:@(section)];
 
-    [self scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:section]
-                atScrollPosition:UITableViewScrollPositionTop
-                        animated:animated];
-
     void(^completionBlock)(void) = ^{
         if ([self respondsToSelector:@selector(scrollViewDidScroll:)]) {
             [self scrollViewDidScroll:self];
